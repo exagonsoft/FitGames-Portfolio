2fd37c29faa74389bb61da0af598b5d3a0a4a5ff
melkor@aleph.engineering <melkor@aleph.engineering>
2023-07-21 18:40:01 -0400

    #4 :traffic_light: ***Plans Section*** <br />
    :rotating_light: **Problem**
    -
    
    :tools: **Solution**
    - Implement Plans Section
    
    :bookmark_tabs: **Files Changed**
    -
    
    :hourglass: **Time spent on task**<br />
    :microscope: *Research* : 0 h 0 min <br />
    :computer: *Develop* : 0 h 0 min <br />
    :outbox_tray: *Deploy* : 0 h 0 min <br />

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./plans.css";
+import { plansData } from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
+
+const Plans = () => {
+  return (
+    <div className="plans">
+      <div className="plans-header">
+        <span className="stroke-text">Ready to start</span>
+        <span>your jurney</span>
+        <span className="stroke-text">now withus</span>
+      </div>
+      {/**Plans Header*/}
+      <div className="plans-cards">
+        {plansData.map((plan, id) => (
+          <div key={id} className="plan">
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>${plan.price}</span>
+            <div className="features">
+              {plan.features.map((feature, id) => (
+                <div className="feature" key={id}>
+                  <img src={whiteTick} alt="" />
+                  <span>{feature}</span>
+                </div>
+              ))}
+            </div>
+            <div><span>see more benefists -></span></div>
+            <button className="button">Join Now</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Plans;
