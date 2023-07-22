e076663433d97af1446fc5cd10627dc8d39fe513
melkor@aleph.engineering <melkor@aleph.engineering>
2023-07-21 23:03:24 -0400

    #6 :traffic_light: ***Join Section*** <br />
    :rotating_light: **Problem**
    -
    
    :tools: **Solution**
    - Implement Join Section
    
    :bookmark_tabs: **Files Changed**
    -
    
    :hourglass: **Time spent on task**<br />
    :microscope: *Research* : 0 h 0 min <br />
    :computer: *Develop* : 0 h 0 min <br />
    :outbox_tray: *Deploy* : 0 h 0 min <br />

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+import {} from "./join.css";
+import emailjs from "@emailjs/browser";
+
+const Join = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_skqalqp",
+        "template_x1c23tf",
+        form.current,
+        "Wnhg0tFngoA1oBIWe"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="join" id="join">
+      <div className="left-j">
+        <hr></hr>
+        <div>
+          <span className="stroke-text">READY TO</span>
+          <span>LEVEL UP</span>
+        </div>
+        <div>
+          <span>YOUR BODY</span>
+          <span className="stroke-text">WITH US?</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form ref={form} className="email-containner" onSubmit={sendEmail}>
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Enter your Email"
+          />
+          <button className="button btn-j">Join Now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
