b18af8f100928960cc96d82b9f5cd4acf34ab378
melkor@aleph.engineering <melkor@aleph.engineering>
2023-07-22 14:24:57 -0400

    #12 :traffic_light: ***Add Navigation*** <br />
    :rotating_light: **Problem**
    # Problem, Task, Reason for CommitSolution
    
    :tools: **Solution**
    - Implement the Menu Navigation function
    
    :bookmark_tabs: **Files Changed**
    # List of Modified Files
    
    :hourglass: **Time spent on task**<br />
    :microscope: *Research* : 0 h 0 min <br />
    :computer: *Develop* : 0 h 0 min <br />
    :outbox_tray: *Deploy* : 0 h 0 min <br />

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,28 +2,67 @@ import React, { useState } from "react";
 import "./Header.css";
 import fglogo from "../../assets/fglogo.png";
 import bars from "../../assets/bars.png";
+import { Link } from "react-scroll";
 
 const Header = () => {
   const inMobile = window.innerWidth <= 768 ? true : false;
   const [showMenu, setShowMenu] = useState(false);
 
   const ToggleMenu = () => {
-    setShowMenu(!showMenu)
-  }
+    setShowMenu(!showMenu);
+  };
   return (
     <div className="header">
+      {inMobile ? <></> : <div className="btn-to-top"><Link to="header" spy={true} smooth={true}>UP</Link></div>}
+
       <img src={fglogo} alt="Logo" className="logo"></img>
       {showMenu === false && inMobile === true ? (
-        <div onClick={ToggleMenu} style={{backgroundColor:'var(--appColor)', padding:'5px', WebkitBorderRadius:'5px'}}>
-          <img src={bars} alt="" style={{width:'1.5rem', height:'1.5rem'}}/>
+        <div
+          onClick={ToggleMenu}
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "5px",
+            WebkitBorderRadius: "5px",
+          }}
+        >
+          <img
+            src={bars}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
         </div>
       ) : (
         <ul className="header-menu">
-          <li onClick={ToggleMenu}>Home</li>
-          <li onClick={ToggleMenu}>Programs</li>
-          <li onClick={ToggleMenu}>Why Us</li>
-          <li onClick={ToggleMenu}>Plans</li>
-          <li onClick={ToggleMenu}>Testimonials</li>
+          <li>
+            <Link onClick={ToggleMenu} to="header" spy={true} smooth={true}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link onClick={ToggleMenu} to="programs" spy={true} smooth={true}>
+              Programs
+            </Link>
+          </li>
+          <li>
+            <Link onClick={ToggleMenu} to="reasons" spy={true} smooth={true}>
+              Why Us
+            </Link>
+          </li>
+          <li>
+            <Link onClick={ToggleMenu} to="plans" spy={true} smooth={true}>
+              Plans
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={ToggleMenu}
+              to="testimonials"
+              spy={true}
+              smooth={true}
+            >
+              Testimonials
+            </Link>
+          </li>
         </ul>
       )}
     </div>
