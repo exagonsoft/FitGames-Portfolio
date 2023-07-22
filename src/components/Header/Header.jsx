98f65b5d8cafebfe1a8843ce8bddeb1e76fd6bad
melkor@aleph.engineering <melkor@aleph.engineering>
2023-07-22 13:53:13 -0400

    #11 :traffic_light: ***Implement Side Bar*** <br />
    :rotating_light: **Problem**
    # Problem, Task, Reason for CommitSolution
    
    :tools: **Solution**
    - Implement the App side Bar
    
    :bookmark_tabs: **Files Changed**
    # List of Modified Files
    
    :hourglass: **Time spent on task**<br />
    :microscope: *Research* : 0 h 0 min <br />
    :computer: *Develop* : 0 h 0 min <br />
    :outbox_tray: *Deploy* : 0 h 0 min <br />

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
-import  fglogo  from "../../assets/fglogo.png";
+import fglogo from "../../assets/fglogo.png";
+import bars from "../../assets/bars.png";
 
 const Header = () => {
+  const inMobile = window.innerWidth <= 768 ? true : false;
+  const [showMenu, setShowMenu] = useState(false);
+
+  const ToggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
   return (
     <div className="header">
-      <img src={fglogo} alt="Logo" className="logo"></img> 
-      <ul className="header-menu">
-        <li>Home</li>
-        <li>Programs</li>
-        <li>Why Us</li>
-        <li>Plans</li>
-        <li>Testimonials</li>
-      </ul>
+      <img src={fglogo} alt="Logo" className="logo"></img>
+      {showMenu === false && inMobile === true ? (
+        <div onClick={ToggleMenu} style={{backgroundColor:'var(--appColor)', padding:'5px', WebkitBorderRadius:'5px'}}>
+          <img src={bars} alt="" style={{width:'1.5rem', height:'1.5rem'}}/>
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li onClick={ToggleMenu}>Home</li>
+          <li onClick={ToggleMenu}>Programs</li>
+          <li onClick={ToggleMenu}>Why Us</li>
+          <li onClick={ToggleMenu}>Plans</li>
+          <li onClick={ToggleMenu}>Testimonials</li>
+        </ul>
+      )}
     </div>
   );
 };
